f433829b3c6419fd391a2d7a1a770c6f324672a5
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-06 06:24:14 -0700

    feat: toast plus banner state when an override fails

--- app/src/main/java/io/nekohasekai/sfa/utils/OverrideStatus.kt
+++ app/src/main/java/io/nekohasekai/sfa/utils/OverrideStatus.kt
@@ -1,5 +1,9 @@
 package io.nekohasekai.sfa.utils
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import io.nekohasekai.sfa.Application
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -19,9 +23,15 @@ object OverrideStatus {
 
     fun set(items: List<OverrideNotice>) {
         _notices.value = items
+        if (items.isNotEmpty()) {
+            val text = items.joinToString("\n\n") { "${it.title}\n${it.reason}\n${it.hint}" }
+            Handler(Looper.getMainLooper()).post {
+                Toast.makeText(Application.application, text, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     fun add(item: OverrideNotice) {
-        _notices.value = _notices.value + item
+        set(_notices.value + item)
     }
 }
